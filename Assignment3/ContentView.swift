1d6e068bfcde693c948ca503e977526d336f6de8
mattwbdv <koenigmattd@gmail.com>
2023-05-23 18:20:30 -0700

    working app with mock data

--- Assignment3/ContentView.swift
+++ Assignment3/ContentView.swift
@@ -1,26 +1,61 @@
-//
-// ContentView.swift : Assignment3
-//
-// Copyright © 2023 Auburn University.
-// All Rights Reserved.
-
-
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
+    @State private var hackerNewsPosts: [HackerNewsPost] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(hackerNewsPosts) { post in
+                NavigationLink(destination: WebView(url: URL(string: post.url)!)) {
+                    VStack(alignment: .leading) {
+                        Text(post.title)
+                            .font(.headline)
+                        Text(post.url)
+                            .font(.subheadline)
+                    }
+                }
+            }
+            .onAppear {
+                fetchHackerNewsPosts()
+            }
+            .padding()
+            .navigationTitle("Hacker News")
+            .background(Color.orange) // Set the background color of NavigationView
         }
-        .padding()
     }
+
+    private func fetchHackerNewsPosts() {
+        hackerNewsPosts = [
+            HackerNewsPost(title: "Sample Post 1", url: "https://example.com/1"),
+            HackerNewsPost(title: "Sample Post 2", url: "https://example.com/2"),
+            //...
+        ]
+    }
+}
+
+struct HackerNewsPost: Identifiable {
+    var id = UUID()
+    var title: String
+    var url: String
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView  {
+        let webView = WKWebView()
+        webView.load(URLRequest(url: url))
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        // Nothing to do here
     }
 }
