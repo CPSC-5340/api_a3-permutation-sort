8184101750fbd0b7ae49dceb8fdfb0e0d4b89338
mattwbdv <koenigmattd@gmail.com>
2023-05-23 18:24:43 -0700

    feed now working

--- Assignment3/ContentView.swift
+++ Assignment3/ContentView.swift
@@ -21,21 +21,63 @@ struct ContentView: View {
             }
             .padding()
             .navigationTitle("Hacker News")
-            .background(Color.orange) // Set the background color of NavigationView
+            .background(Color.orange)
         }
     }
 
     private func fetchHackerNewsPosts() {
-        hackerNewsPosts = [
-            HackerNewsPost(title: "Sample Post 1", url: "https://example.com/1"),
-            HackerNewsPost(title: "Sample Post 2", url: "https://example.com/2"),
-            //...
-        ]
+        guard let url = URL(string: "https://hacker-news.firebaseio.com/v0/topstories.json") else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                print("Error fetching Hacker News posts: \(error)")
+                return
+            }
+            
+            if let data = data {
+                do {
+                    let postIDs = try JSONDecoder().decode([Int].self, from: data)
+                    
+                    let postIDsSlice = postIDs.prefix(10)
+                    let group = DispatchGroup()
+                    
+                    for postID in postIDsSlice {
+                        group.enter()
+                        
+                        let postURL = URL(string: "https://hacker-news.firebaseio.com/v0/item/\(postID).json")!
+                        
+                        URLSession.shared.dataTask(with: postURL) { postData, _, _ in
+                            defer { group.leave() }
+                            
+                            if let postData = postData {
+                                do {
+                                    let post = try JSONDecoder().decode(HackerNewsPost.self, from: postData)
+                                    DispatchQueue.main.async {
+                                        hackerNewsPosts.append(post)
+                                    }
+                                } catch {
+                                    print("Error decoding post data: \(error)")
+                                }
+                            }
+                        }.resume()
+                    }
+                    
+                    group.notify(queue: DispatchQueue.main) {
+                        // All posts fetched
+                    }
+                } catch {
+                    print("Error decoding post IDs: \(error)")
+                }
+            }
+        }.resume()
     }
+
 }
 
-struct HackerNewsPost: Identifiable {
-    var id = UUID()
+struct HackerNewsPost: Identifiable, Decodable {
+    var id: Int
     var title: String
     var url: String
 }
