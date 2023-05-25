98027dcfd1c7a1b63f0e61fd5f8b72b034104d5e
mattwbdv <koenigmattd@gmail.com>
2023-05-25 16:41:29 -0700

    mvvc refactor

--- Assignment3/ContentView.swift
+++ Assignment3/ContentView.swift
@@ -1,8 +1,7 @@
 import SwiftUI
-import WebKit
 
 struct ContentView: View {
-    @State private var hackerNewsPosts: [HackerNewsPost] = []
+    @ObservedObject var viewModel = HackerNewsViewModel()
     @State private var showWebView = false
     @State private var webViewURL: URL?
     
@@ -15,7 +14,7 @@ struct ContentView: View {
             
             ScrollView {
                 VStack(spacing: 10) {
-                    ForEach(hackerNewsPosts) { post in
+                    ForEach(viewModel.hackerNewsPosts) { post in
                         Button(action: {
                             if let url = post.url {
                                 webViewURL = URL(string: url)
@@ -49,79 +48,10 @@ struct ContentView: View {
         }
         .background(Color.orange.ignoresSafeArea())
         .onAppear {
-            fetchHackerNewsPosts()
+            viewModel.fetchHackerNewsPosts()
         }
         .sheet(isPresented: $showWebView) {
             WebView(url: webViewURL)
         }
     }
-
-    private func fetchHackerNewsPosts() {
-        guard let url = URL(string: "https://hacker-news.firebaseio.com/v0/topstories.json") else {
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            if let error = error {
-                print("Error fetching Hacker News posts: \(error)")
-                return
-            }
-            
-            if let data = data {
-                do {
-                    let postIDs = try JSONDecoder().decode([Int].self, from: data)
-                    
-                    let postIDsSlice = postIDs.prefix(10)
-                    let group = DispatchGroup()
-                    
-                    for postID in postIDsSlice {
-                        group.enter()
-                        
-                        let postURL = URL(string: "https://hacker-news.firebaseio.com/v0/item/\(postID).json")!
-                        
-                        URLSession.shared.dataTask(with: postURL) { postData, _, _ in
-                            defer { group.leave() }
-                            
-                            if let postData = postData {
-                                do {
-                                    let post = try JSONDecoder().decode(HackerNewsPost.self, from: postData)
-                                    DispatchQueue.main.async {
-                                        hackerNewsPosts.append(post)
-                                    }
-                                } catch {
-                                    print("Error decoding post data: \(error)")
-                                }
-                            }
-                        }.resume()
-                    }
-                    
-                    group.notify(queue: DispatchQueue.main) {
-                        // All posts fetched
-                    }
-                } catch {
-                    print("Error decoding post IDs: \(error)")
-                }
-            }
-        }.resume()
-    }
-}
-
-struct WebView: UIViewRepresentable {
-    let url: URL?
-    func makeUIView(context: Context) -> WKWebView {
-        return WKWebView()
-    }
-
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        if let url = url {
-            let request = URLRequest(url: url)
-            uiView.load(request)
-        }
-    }
-}
-
-struct HackerNewsPost: Identifiable, Decodable {
-var id: Int
-var title: String
-var url: String?
 }
